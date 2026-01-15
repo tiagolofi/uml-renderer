ec3f1324f012907178acfe4b4e9ee3955294b0bb
tiagolofi <jejeco75@gmail.com>
2026-01-15 20:35:55 -0300

    configuração inicial

---
+++ build.sh
@@ -0,0 +1,3 @@
+python -m pip install --upgrade pip
+python -m pip install --upgrade build
+python -m build
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="uml-renderer",
+    version="0.0.1",
+    description="",
+    author="tiagolofi",
+    author_email="tiagolofi@example.com",
+    url="https://github.com/tiagolofi/uml-renderer",
+    packages=find_packages(),
+    python_requires=">=3.9",
+    install_requires=[
+        "python-dotenv", "pytest"
+    ],
+    extras_require={
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
